c071848832ef024846fbfdab7be3735e0c7ebeae
Allen890 <476363910@qq.com>
2019-08-28 16:53:47 +0800

    登录页

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,23 +1,85 @@
 <template>
-  <div class="index">
+  <el-container class="index">
+    <el-header>
+      <div class="img">
+        <img src="../assets/logo.png" alt />
+      </div>
+
+      <div class="system">
+        <h1>电商后台管理系统</h1>
+      </div>
+
+      <div class="logout">
+        <span>
+          欢迎光临~
+          <a href="javascript:;">退出</a>
+        </span>
+      </div>
+    </el-header>
     <el-container>
-      <el-header>Header</el-header>
-      <el-container>
-        <el-aside width="200px">Aside</el-aside>
-        <el-main>Main</el-main>
-      </el-container>
+      <el-aside width="200px">
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>导航一</span>
+            </template>
+            <el-menu-item-group>
+              <template slot="title">分组一</template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="1-4-1">选项1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-menu-item index="2">
+            <i class="el-icon-menu"></i>
+            <span slot="title">导航二</span>
+          </el-menu-item>
+          <el-menu-item index="3" disabled>
+            <i class="el-icon-document"></i>
+            <span slot="title">导航三</span>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <i class="el-icon-setting"></i>
+            <span slot="title">导航四</span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+      <el-main>Main</el-main>
     </el-container>
-  </div>
+  </el-container>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style>
-.el-header {
-  width: 100%;
-  height: 60px;
-  background-color: #d8d8d8;
+<style lang='scss' scoped>
+.index {
+  height: 100%;
+  .el-header {
+    background-color: #d8d8d8;
+    display: flex;
+    .img {
+      height: 40px;
+    }
+  }
+  .el-aside {
+    background-color: #545c64;
+  }
 }
 </style>
